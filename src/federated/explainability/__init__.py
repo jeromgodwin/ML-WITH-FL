4b04ef80bfaf2adc932e540307c3dd242910578b
jeromgodwin <jeromgodwin@gmail.com>
2026-08-22 20:13:49 +0530

    Enhancement 4: explainability package init

---
+++ src/federated/explainability/__init__.py
@@ -0,0 +1,5 @@
+"""Explainable malware detection (Enhancement 4)."""
+
+from src.federated.explainability.explainer import FeatureExplainer, explain_prediction
+
+__all__ = ["FeatureExplainer", "explain_prediction"]
